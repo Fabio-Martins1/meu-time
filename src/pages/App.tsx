f74c7a76b8ab452705777bfccf54f0ae63440d73
Fabio Martins <kiritosannee@gmail.com>
2023-05-20 15:21:08 -0300

    Implementado map dos selects

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,68 @@
 import { Select } from 'antd'
-import './App.css'
+import styles from "./App.module.scss"
 
 function App() {
+  interface IOption{
+    value:string,
+    label:string
+  }
+  interface ISelect {
+    placeholder:string,
+    options:IOption[]
+  }
+  const listaSelect:ISelect[] = [
+    {
+      placeholder:"País",
+      options:[{
+        value:"brazil",
+        label: "Brasil"
+      },
+      {
+        value:"spain",
+        label: "Espanha"
+      }
+    ]
+    },
+    {placeholder:"Liga",
+    options:[{
+      value:"brazilian",
+      label: "Liga Brasileira"
+    },
+    {
+      value:"spanish",
+      label: "Liga Espanhola"
+    }
+  ]},
+    {placeholder:"Clube",
+    options:[{
+      value:"ceara",
+      label: "Ceará"
+    },
+    {
+      value:"vasco",
+      label: "Vasco"
+    }
+  ]}
+  ]
 
   return (
-    <div id="container-dashboard-meu-time" className="container">
-      <div id="container-selecao-meu-time" className="container-selecao">
-        <Select options={[
-          {value:"Brazil",label:"Brasil"},
-          {value:"Spain",label:"Espanha"}
-        ]}>
-        </Select>
+    <div id={"container-dashboard-meu-time"} className={styles.container}>
+      <div id={"container-selecao-meu-time"} className={styles.containerSelecao}>
+        {listaSelect.map((select) => {
+          return(
+          <Select
+          id={"select-meu-time"}
+          style={{ width: 200,paddingTop: 5 }}
+          placeholder={select.placeholder}
+          >
+            {select.options.map((option) => {
+              return(
+                <option value={option.value}>{option.label}</option>
+              )
+            })}
+          </Select>
+          )
+        })}
       </div>
     </div>
   )
